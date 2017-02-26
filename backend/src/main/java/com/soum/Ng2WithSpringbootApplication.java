dd6ded4dc5cb6fdcde37c5937a104780ed6840f1
soumx <soumyaditya@gmail.com>
2017-02-26 12:41:28 -0800

    Add files via upload

---
+++ backend/src/main/java/com/soum/Ng2WithSpringbootApplication.java
@@ -0,0 +1,12 @@
+package com.soum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ng2WithSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ng2WithSpringbootApplication.class, args);
+	}
+}
---
+++ backend/src/test/java/com/soum/Ng2WithSpringbootApplicationTests.java
@@ -0,0 +1,16 @@
+package com.soum;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Ng2WithSpringbootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
